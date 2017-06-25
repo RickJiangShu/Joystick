fbaab4b385e9855cd85a41c3c8c7daa632263cc7
Rick <rickjiangshu@gmail.com>
2017-06-25 11:00:14 +0800

    add motion

--- Joystick.cs
+++ Joystick.cs
@@ -4,7 +4,6 @@ using System;
 using UnityEngine;
 
 public class Joystick : MonoBehaviour {
-
     //事件
     public Action OnTouchDown;
     public Action<float,float> OnTouchMove;//p1:angle360 0~360 p2:power 0~1
@@ -13,21 +12,35 @@ public class Joystick : MonoBehaviour {
     //Untiy 指定参数
     public GameObject control;//移动对象
     public float controlRadius = 200.0f;//移动半径（UGUI像素）
+    public Rect touchArea = new Rect(0, 0, 1f, 1f);//0~1
+    public float replaceTime = 0.1f;
     
-    private Vector3 touchOrigin;//按下原点（转换到Canvas的点）
+    private Vector3 touchOrigin;//按下原点（Input.mousePosition）
     private float scaleFactor;//Screen 2 Canvas 的转换因子
-    private Vector3 controlOrigin;//移动对象的原点
+
+    private Transform self;
+    private Vector3 selfDefaultPosition;
+    private Vector3 ctrlDefaultLocalPos;//control的默认位置
+
+    private bool isOnArea = false;//是否点击在区域上
+    private bool isDragged = false;//是否正在拖拽
+    private bool isReplace = false;//是否正在复位
+
+    //复位
+    private float replaceCount = 0f;
+    private Vector3 selfReplaceSpd;
+    private Vector3 ctrlReplaceSpd;
 
 	// Use this for initialization
 	void Start () {
-        if (control != null)
-        {
-            //获取转换系数
-            Canvas canvas = control.GetComponentInParent<Canvas>();
-            scaleFactor = canvas.scaleFactor;
+        self = transform;
+        selfDefaultPosition = self.position;
 
-            controlOrigin = control.transform.localPosition;
-        }
+        //获取转换系数
+        Canvas canvas = control.GetComponentInParent<Canvas>();
+        scaleFactor = canvas.scaleFactor;
+
+        ctrlDefaultLocalPos = control.transform.localPosition;
 	}
 	
 	// Update is called once per frame
@@ -49,21 +62,53 @@ public class Joystick : MonoBehaviour {
         {
             TouchUp();
         }
+
+        //复位
+        if (isReplace)
+        {
+            replaceCount += Time.deltaTime;
+            if (replaceCount < replaceTime)
+            {
+                self.position += selfReplaceSpd * Time.deltaTime;
+                control.transform.localPosition += ctrlReplaceSpd * Time.deltaTime;
+            }
+            else
+            {
+                self.position = selfDefaultPosition;
+                control.transform.localPosition = ctrlDefaultLocalPos;
+            }
+        }
 	}
 
     private void TouchDown()
     {
-        touchOrigin = Input.mousePosition / scaleFactor;
+        Vector3 touchPosition = Input.mousePosition;
+        Vector2 touchScreen = new Vector2(touchPosition.x / Screen.width, touchPosition.y / Screen.height);
+        isOnArea = touchArea.Contains(touchScreen);
+        if (!isOnArea)
+            return;
+
+        isReplace = false;
+        touchOrigin = touchPosition;
+        self.position = touchOrigin;
 
         if (OnTouchDown != null)
             OnTouchDown();
     }
     private void TouchMove()
     {
-        Vector3 now = Input.mousePosition / scaleFactor;
+        if (!isOnArea)
+            return;
 
-        float distance = Vector3.Distance(now, touchOrigin);
-        Vector3 direction = now - touchOrigin;
+        Vector3 touch = touchOrigin / scaleFactor;
+        Vector3 now = Input.mousePosition / scaleFactor;
+        float distance = Vector3.Distance(now, touch);
+        if (distance < 0.01f)
+            return;
+
+        isDragged = true;
+
+        Vector3 direction = now - touch;
         float radians = Mathf.Atan2(direction.y, direction.x);
 
         //移动摇杆
@@ -74,7 +119,7 @@ public class Joystick : MonoBehaviour {
 
             float mx = Mathf.Cos(radians) * distance;
             float my = Mathf.Sin(radians) * distance;
-            Vector3 uiPos = controlOrigin;
+            Vector3 uiPos = ctrlDefaultLocalPos;
             uiPos.x += mx;
             uiPos.y += my;
             control.transform.localPosition = uiPos;
@@ -92,7 +137,21 @@ public class Joystick : MonoBehaviour {
     }
     private void TouchUp()
     {
-        control.transform.localPosition = controlOrigin;
+        isOnArea = false;
+        isDragged = false;
+        if (replaceTime > 0f)
+        {
+            isReplace = true;
+            replaceCount = 0f;
+            selfReplaceSpd = (selfDefaultPosition - self.position) / replaceTime;
+            ctrlReplaceSpd = (ctrlDefaultLocalPos - control.transform.localPosition) / replaceTime;
+        }
+        else
+        {
+            self.position = selfDefaultPosition;
+            control.transform.localPosition = ctrlDefaultLocalPos;
+        }
+
 
         if (OnTouchUp != null)
             OnTouchUp();
